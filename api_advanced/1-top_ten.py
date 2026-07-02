aac7c79dc107bf99cb722d7a5d0fa24b35f074ec
Noel Lancelot <enzoasaph5@gmail.com>
2026-07-02 13:02:04 +0300

    Use partner's exact api_advanced implementations

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
-"""Module that queries the Reddit API for top 10 hot posts."""
+"""Prints the titles of the first 10 hot posts listed for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Print titles of the first 10 hot posts for a subreddit."""
+    """Queries Reddit API and prints titles of first 10 hot posts."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        "User-Agent": "python:alu.api.advanced:v1.0 (by /u/alu_student)"
-    }
+    headers = {"User-Agent": "mozilla/5.0"}
     params = {"limit": 10}
+
     try:
-        response = requests.get(url, headers=headers, params=params,
-                                allow_redirects=False, verify=False)
-        if response.status_code != 200:
-            print(None)
-            return
-        posts = response.json().get("data", {}).get("children", [])
-        for post in posts:
-            print(post.get("data", {}).get("title"))
+        response = requests.get(
+            url, headers=headers, params=params, allow_redirects=False
+        )
     except Exception:
+        return
+
+    if response.status_code != 200:
         print(None)
+        return
+
+    data = response.json()
+    posts = data.get("data", {}).get("children", [])
+
+    if not posts:
+        return
+
+    for post in posts:
+        title = post.get("data", {}).get("title")
+        if title:
+            print(title)
--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""Module that recursively queries Reddit API for all hot post titles."""
+""" 2-recurse.py """
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """Return list of titles of all hot articles for a subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        "User-Agent": "python:alu.api.advanced:v1.0 (by /u/alu_student)"
-    }
-    params = {"limit": 100}
-    if after:
-        params["after"] = after
-    try:
-        response = requests.get(url, headers=headers, params=params,
-                                allow_redirects=False, verify=False)
-    except Exception:
+    """ returns list with titles of all hot articles in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    params = {"after": after}
+    response = requests.get(
+                            url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()["data"]
+        hot_list += [post["data"]["title"] for post in data["children"]]
+        if data["after"] is None:
+            return hot_list
+        else:
+            return recurse(subreddit, hot_list, data["after"])
+    elif response.status_code == 404:
         return None
-    if response.status_code != 200:
-        return None
-    data = response.json().get("data", {})
-    posts = data.get("children", [])
-    after = data.get("after")
-    for post in posts:
-        hot_list.append(post.get("data", {}).get("title"))
-    if after:
-        return recurse(subreddit, hot_list, after)
-    return hot_list
