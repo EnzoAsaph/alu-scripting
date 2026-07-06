453abb9ce9485b207a6c42ed07a007e5064286f6
EnzoAsaph <enzoasaph5@gmail.com>
2026-07-06 18:06:57 +0300

    api_advanced: use requests module and stop following redirects in 1-top_ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""Module that queries the Reddit API for top 10 hot posts."""
-import json
-import urllib.request
+"""Module that queries the Reddit API for the first 10 hot posts."""
+import requests
 
 
 def top_ten(subreddit):
     """Print titles of the first 10 hot posts for a subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "python:alu.api.advanced:v1.0 (by /u/alu)"}
-    req = urllib.request.Request(url, headers=headers)
-    try:
-        with urllib.request.urlopen(req) as r:
-            data = json.loads(r.read().decode('utf-8'))
-            posts = data.get("data", {}).get("children", [])
-            if not posts:
-                print(None)
-                return
-            for post in posts:
-                print(post.get("data", {}).get("title"))
-    except Exception:
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code != 200:
         print(None)
+        return
+    posts = response.json().get("data", {}).get("children", [])
+    if not posts:
+        print(None)
+        return
+    for post in posts:
+        print(post.get("data", {}).get("title"))
